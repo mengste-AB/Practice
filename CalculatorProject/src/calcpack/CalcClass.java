5aebdeb04673a77541bdd1e5143c9a6bcf22ec75
mengsteab2019@gmail.com <mengsteab2019@gmail.com>
2021-04-15 22:53:10 -0400

    first commit

---
+++ CalculatorProject/src/calcpack/CalcClass.java
@@ -0,0 +1,17 @@
+package calcpack;
+
+public class CalcClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there");
+		String  name = "jemes";
+		int age = 30;
+		System.out.println("my name is " +name);
+		System.out.println("my age is " +age);
+
+		
+
+	}
+
+}
