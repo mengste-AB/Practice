1d505a105ee82f2890119341cd490499070b2065
mengsteab2019@gmail.com <mengsteab2019@gmail.com>
2021-04-16 20:48:35 -0400

    "new add"

---
+++ Gittest/src/gitpack/GitClass.java
@@ -0,0 +1,12 @@
+package gitpack;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "git";
+		System.out.println(name);
+
+	}
+
+}
